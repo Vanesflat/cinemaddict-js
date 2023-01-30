8ac298517a0aecbc25473cfdf5d5e84597aa2273
Vanesflat <ptptcptptc@mail.ru>
2023-01-30 15:10:19 +0700

    Added functions for generate mock data

---
+++ src/utils/common.js
@@ -0,0 +1,23 @@
+const getRandomArrayElement = (items) => items[Math.floor(Math.random() * items.length)];
+
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const getRandomElementsArray = (arr, length) => {
+  const values = [];
+  while (values.length < length) {
+    const currentElement = getRandomArrayElement(arr);
+    values.push(currentElement);
+  }
+  return values;
+};
+
+const getRandomFractNumber = (min, max, decimalPlaces) => {
+
+  if (Math.sign(max) === -1 || Math.sign(min) === -1 || max === min || max < min) {
+    return NaN;
+  }
+
+  return (Math.random() * (max - min) + min).toFixed(decimalPlaces);
+};
+
+export { getRandomArrayElement, getRandomInteger, getRandomElementsArray, getRandomFractNumber };
