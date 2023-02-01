cff50c5062ec7e10110b4098a89dc0d1fb0499e0
Vanesflat <ptptcptptc@mail.ru>
2023-02-01 11:58:19 +0700

    Realized popup actions

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,4 +1,5 @@
 import { render } from '../render.js';
+import { isEscapeKey } from '../utils/film.js';
 import BoardFilmsView from '../view/board-films-view.js';
 import FilmCardView from '../view/film-card-view.js';
 import ListFilmsContainerView from '../view/list-films-container-view.js';
@@ -29,11 +30,49 @@ export default class BoardPresenter {
     render(this.#filmListContainer, this.#filmListComponent.element);
 
     for (let i = 0; i < this.#films.length; i++) {
-      render(new FilmCardView(this.#films[i]), this.#filmListContainer.element);
+      this.#renderFilm(this.#films[i]);
     }
 
     render(new ShowMoreButtonView(), this.#filmListComponent.element);
 
-    render(new PopupView(this.#films[0]), document.body);
+  }
+
+  #renderFilm(film) {
+    const filmComponent = new FilmCardView(film);
+    const popupComponent = new PopupView(film);
+
+    const replaceCardToPopup = () => {
+      document.body.appendChild(popupComponent.element);
+      document.body.classList.add('hide-overflow');
+    };
+
+    const replacePopupToCard = () => {
+      document.body.removeChild(popupComponent.element);
+      document.body.classList.remove('hide-overflow');
+    };
+
+    const escapeKeydownHandler = (evt) => {
+      if (isEscapeKey(evt)) {
+        evt.preventDefault();
+        replacePopupToCard();
+        document.removeEventListener('keydown', escapeKeydownHandler);
+      }
+    };
+
+    filmComponent.element.querySelector('.film-card__link').addEventListener('click', (evt) => {
+      evt.preventDefault();
+      replaceCardToPopup();
+      document.addEventListener('keydown', escapeKeydownHandler);
+    });
+
+    popupComponent.element.querySelector('.film-details__close-btn').addEventListener('click', (evt) => {
+      if (evt.target.closest('.film-details__top-container')) {
+        evt.preventDefault();
+        replacePopupToCard();
+        document.removeEventListener('keydown', escapeKeydownHandler);
+      }
+    });
+
+    render(filmComponent, this.#filmListContainer.element);
   }
 }
